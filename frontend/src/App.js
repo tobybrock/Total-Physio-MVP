b68b2416727cf597a39b0b28a1b6e6ac2d69f131
tobybrock <tobybrocklehurst6@gmail.com>
2021-08-27 01:32:30 +1000

    added dynamic rating system and sectoned components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import "./index.css";
+import Product from "./components/Product";
 import data from "./data";
+
 //need to section app into components
 
 function App() {
@@ -19,44 +21,12 @@ function App() {
             <a href="/signin">Sign In</a>
           </div>
         </header>
-        
+      
         <main>
           <div>
             <div className="row center">
               {data.products.map((product) => (
-                <div key={product._id} className="card">
-                  <a href={`/product/${product._id}`}>
-                    <img
-                      className="medium"
-                      src={product.image}
-                      alt={product.name}
-                    />
-                  </a>
-                  <div className="card-body">
-                    <a href={`/product/${product._id}`}>
-                      <h2>{product.name}</h2>
-                    </a>
-                    {/*make dynamic ratings */}
-                    <div className="rating">
-                      <span>
-                        <i className="fa fa-star"></i>
-                      </span>
-                      <span>
-                        <i className="fa fa-star"></i>
-                      </span>
-                      <span>
-                        <i className="fa fa-star"></i>
-                      </span>
-                      <span>
-                        <i className="fa fa-star"></i>
-                      </span>
-                      <span>
-                        <i className="fa fa-star"></i>
-                      </span>
-                    </div>
-                    <div className="price">${product.price}</div>
-                  </div>
-                </div>
+                <Product key={product._id} product={product}></Product>
               ))}
             </div>
           </div>
